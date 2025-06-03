0e2bd66f1748bb0da472190e525bd43dfd7c4223
Orion Edge <yhiamdan@gmail.com>
2025-06-03 01:20:35 +0000

    #CA-12 Refactor: Update core module and storage services

--- lib/core/libraries/storage/interfaces/storage_service.dart
+++ lib/core/libraries/storage/interfaces/storage_service.dart
@@ -1,6 +1,6 @@
 /// Storage service interface, used to store and retrieve data from the device's storage.
 /// This is a singleton class, so it can be accessed from anywhere in the app.
-abstract class IStorageService {
+abstract class StorageService {
   /// Initialize the storage service, Some storage services may need to be initialized before use.
   Future<void> initialize();
 
