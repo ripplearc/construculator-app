99de4acc3f665e7b422cc9d78619b8b094130110
Orion Edge <yhiamdan@gmail.com>
2025-06-04 11:20:59 +0000

    Update dotevn_loader to be more consistent with convention

--- lib/libraries/config/dotenv_loader.dart
+++ lib/libraries/config/dotenv_loader.dart
@@ -2,7 +2,7 @@ import 'package:construculator/libraries/config/interfaces/env_loader.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // Default implementation of EnvLoader using the flutter_dotenv package
-class DotEnvLoaderImpl implements EnvLoader {
+class DotEnvLoader implements EnvLoader {
   @override
   Future<void> load({String? fileName}) async {
     if (fileName == null) {
