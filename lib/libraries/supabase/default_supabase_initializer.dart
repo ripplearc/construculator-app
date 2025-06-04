357e4ed4ff1fc55423faa09a90bbbb5c94deea75
Orion Edge <yhiamdan@gmail.com>
2025-06-04 10:52:58 +0000

    Refactor supabase library to be more consistent with conventions

--- lib/libraries/supabase/default_supabase_initializer.dart
+++ lib/libraries/supabase/default_supabase_initializer.dart
@@ -2,7 +2,7 @@
 import 'package:construculator/libraries/supabase/interfaces/supabase_initializer.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class DefaultSupabaseInitializerImpl implements SupabaseInitializer {
+class DefaultSupabaseInitializer implements SupabaseInitializer {
   @override
   Future<SupabaseClient> initialize({
     required String url,
