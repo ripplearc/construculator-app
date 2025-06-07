ae4dcb02811d9ae556bf3f7c7fe9ef2c731fdd3e
Orion Edge <yhiamdan@gmail.com>
2025-06-07 00:01:02 +0000

    Add auth library module

---
+++ lib/libraries/auth/auth_module.dart
@@ -0,0 +1,20 @@
+import 'package:construculator/libraries/auth/interfaces/auth_repository.dart';
+import 'package:construculator/libraries/auth/repositories/supabase_auth_repository.dart';
+import 'package:construculator/libraries/supabase/interfaces/supabase_wrapper.dart';
+import 'package:construculator/libraries/supabase/supabase_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AuthLibraryModule extends Module {
+  @override
+  List<Module> get imports => [
+    SupabaseModule(),
+  ];
+  @override
+  void exportedBinds(Injector i) {
+    i.addLazySingleton<AuthRepository>(
+      () => SupabaseAuthRepository(
+        supabaseWrapper: i.get<SupabaseWrapper>(),
+      ),
+    );
+  }
+}
