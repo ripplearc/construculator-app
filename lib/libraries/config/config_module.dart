d17f82aa36b345ee5a0ad8fbe3a1f4173f9e09a9
Orion Edge <yhiamdan@gmail.com>
2025-06-07 02:05:13 +0000

    Move supabase initializer implementation to config

--- lib/libraries/config/config_module.dart
+++ lib/libraries/config/config_module.dart
@@ -1,19 +1,16 @@
 import 'package:construculator/libraries/config/app_config.dart';
+import 'package:construculator/libraries/config/default_supabase_initializer.dart';
 import 'package:construculator/libraries/config/dotenv_loader.dart';
 import 'package:construculator/libraries/config/interfaces/config.dart';
 import 'package:construculator/libraries/config/interfaces/env_loader.dart';
-import 'package:construculator/libraries/supabase/supabase_module.dart';
+import 'package:construculator/libraries/supabase/interfaces/supabase_initializer.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class ConfigModule extends Module {
 
   @override
-  List<Module> get imports => [
-    SupabaseModule(),
-  ];
-
-  @override
   void binds(Injector i) {
+    i.addLazySingleton<SupabaseInitializer>(() => DefaultSupabaseInitializer());
     i.addLazySingleton<EnvLoader>(() => DotEnvLoader());
   }
 
--- lib/libraries/supabase/supabase_module.dart
+++ lib/libraries/supabase/supabase_module.dart
@@ -1,11 +1,9 @@
-import 'package:construculator/libraries/supabase/default_supabase_initializer.dart';
-import 'package:construculator/libraries/supabase/interfaces/supabase_initializer.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class SupabaseModule extends Module {
 
   @override
   void exportedBinds(Injector i) {
-    i.addLazySingleton<SupabaseInitializer>(() => DefaultSupabaseInitializer());
+    
   }
 }
