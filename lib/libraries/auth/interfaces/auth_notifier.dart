29027092f2b28bce39f9b3af37c21e64b1b48a76
Orion Edge <yhiamdan@gmail.com>
2025-06-07 14:40:03 +0000

    Update comments for interfaces

--- lib/libraries/auth/interfaces/auth_notifier.dart
+++ lib/libraries/auth/interfaces/auth_notifier.dart
@@ -1,18 +1,21 @@
 import 'package:construculator/libraries/auth/data/models/auth_credential.dart';
 import 'package:construculator/libraries/auth/data/types/auth_types.dart';
 
-// Provides a way to notify other parts of the app about auth state changes.
+/// Provides a way to notify other parts of the app about auth state changes.
+///
+/// [onLogin] is emitted when a user logs in.
+/// [onLogout] is emitted when a user logs out.
+/// [onAuthStateChanged] is emitted when the auth state changes.
+/// [onSetupProfile] is emitted when a user needs to setup their profile.
+/// [emitLogin] is used to emit a login event.
+/// [emitLogout] is used to emit a logout event.
 abstract class AuthNotifier {
   Stream<UserCredential> get onLogin;
   Stream<void> get onLogout;
   Stream<AuthStatus> get onAuthStateChanged;
-  // Consumers can listen to this stream to redirect to setup profile screen.
   Stream<void> get onSetupProfile;
-  
   void emitLogin(UserCredential user);
   void emitLogout();
   void emitAuthStateChanged(AuthStatus status);
-  // Sometimes creating a user profile might fail, if the auth library tries to read the profile info and fails, 
-  // the consumers must be notified to setup their profile.
   void emitSetupProfile();
 } 
