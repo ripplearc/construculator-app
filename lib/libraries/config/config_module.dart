fac42e546bb21b6c73d0dbe6c12d66e00f45c576
Orion Edge <yhiamdan@gmail.com>
2025-06-11 19:50:25 +0000

    Fix restack errors

--- lib/libraries/config/config_module.dart
+++ lib/libraries/config/config_module.dart
@@ -2,22 +2,9 @@ import 'package:construculator/libraries/config/app_config_impl.dart';
 import 'package:construculator/libraries/config/env_loader_impl.dart';
 import 'package:construculator/libraries/config/interfaces/config.dart';
 import 'package:construculator/libraries/config/interfaces/env_loader.dart';
-<<<<<<< HEAD
 import 'package:flutter_modular/flutter_modular.dart';
 
 class ConfigModule extends Module {
-=======
-import 'package:construculator/libraries/config/interfaces/supabase_initializer.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-
-class ConfigModule extends Module {
-  @override
-  void binds(Injector i) {
-    i.addLazySingleton<SupabaseInitializer>(() => DefaultSupabaseInitializer());
-    i.addLazySingleton<EnvLoader>(() => DotEnvLoader());
-  }
-
->>>>>>> fa844ef (Fix restack conflicts)
   @override
   void exportedBinds(Injector i) {
     i.addLazySingleton<EnvLoader>(() => EnvLoaderImpl());
