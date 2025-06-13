4dff2622b1a06021ad778e5055a91a6cbedbfc3f
Orion Edge <yhiamdan@gmail.com>
2025-06-13 11:11:56 +0000

    Rename repo

--- test/units/libraries/auth/supabase_auth_repository_test.dart
+++ test/units/libraries/auth/supabase_auth_repository_test.dart
@@ -5,7 +5,7 @@ import 'package:construculator/libraries/auth/testing/auth_test_module.dart';
 import 'package:construculator/libraries/supabase/data/supabase_types.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:construculator/libraries/auth/repositories/supabase_auth_repository.dart';
+import 'package:construculator/libraries/auth/repositories/supabase_auth_repository_impl.dart';
 import 'package:construculator/libraries/auth/data/types/auth_types.dart';
 import 'package:construculator/libraries/supabase/testing/fake_supabase_wrapper.dart';
 import 'package:supabase_flutter/supabase_flutter.dart' as supabase;
