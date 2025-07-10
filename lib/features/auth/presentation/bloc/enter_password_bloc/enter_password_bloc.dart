59ac6e6047facfc76bf86ad999025865331e0159
nyel <yhiamdan@gmail.com>
2025-07-10 22:00:17 +0000

    Implement enter password bloc

---
+++ lib/features/auth/presentation/bloc/enter_password_bloc/enter_password_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:construculator/libraries/errors/failures.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:construculator/features/auth/domain/usecases/login_usecase.dart';
+
+part 'enter_password_event.dart';
+part 'enter_password_state.dart';
+
+class EnterPasswordBloc extends Bloc<EnterPasswordEvent, EnterPasswordState> {
+  final LoginUseCase _loginUseCase;
+
+  EnterPasswordBloc({required LoginUseCase loginUseCase}) 
+      : _loginUseCase = loginUseCase,
+        super(EnterPasswordInitial()) {
+    on<EnterPasswordSubmitted>(_onSubmitted);
+  }
+
+  Future<void> _onSubmitted(
+    EnterPasswordSubmitted event,
+    Emitter<EnterPasswordState> emit,
+  ) async {
+    emit(EnterPasswordSubmitLoading());
+    final result = await _loginUseCase(event.email, event.password);
+    result.fold(
+      (failure) => emit(EnterPasswordSubmitFailure(failure: failure)),
+      (credential) => emit(EnterPasswordSubmitSuccess()),
+    );
+  }
+} 
