e7a80932c8d132f6a2d8d04854bb16210cba797b
nyel <yhiamdan@gmail.com>
2025-07-10 21:54:16 +0000

    Implement auth domain with usecases

---
+++ lib/features/auth/domain/usecases/send_otp_usecase.dart
@@ -0,0 +1,27 @@
+import 'package:construculator/libraries/auth/data/types/auth_types.dart';
+import 'package:construculator/libraries/auth/interfaces/auth_manager.dart';
+import 'package:dartz/dartz.dart';
+import 'package:construculator/libraries/errors/failures.dart';
+
+/// This is the use case for sending an OTP to the specified address.
+class SendOtpUseCase {
+  final AuthManager authManager;
+
+  SendOtpUseCase(this.authManager);
+
+  /// Sends an OTP to the specified address using the auth manager.
+  /// Accepts an address and an [OtpReceiver] as parameters.
+  /// 
+  /// Returns a [Future] that emits an [Either] containing a [Failure] or an [void].
+  Future<Either<Failure, void>> call(String address, OtpReceiver receiver) async {
+    final result = await authManager.sendOtp(address, receiver);
+    if (!result.isSuccess) {
+      final errorType = result.errorType;
+      if (errorType == null) {
+        return Left(UnexpectedFailure());
+      }
+      return Left(AuthFailure(errorType: errorType));
+    }
+    return Right(null);
+  }
+} 
---
+++ lib/features/auth/domain/usecases/set_new_password_usecase.dart
@@ -0,0 +1,26 @@
+import 'package:construculator/libraries/auth/interfaces/auth_manager.dart';
+import 'package:dartz/dartz.dart';
+import 'package:construculator/libraries/errors/failures.dart';
+
+/// This is the use case for setting a new password.
+class SetNewPasswordUseCase {
+  final AuthManager authManager;
+
+  SetNewPasswordUseCase(this.authManager);
+
+  /// Sets a new password for the specified email using the auth manager.
+  /// Accepts an email and a new password as parameters.
+  /// 
+  /// Returns a [Future] that emits an [Either] containing a [Failure] or an [void].
+  Future<Either<Failure, void>> call(String email,String newPassword) async {
+    final result = await authManager.updateUserCredentials(email, newPassword);
+    if (!result.isSuccess) {
+      final errorType = result.errorType;
+      if (errorType == null) {
+        return Left(UnexpectedFailure());
+      }
+      return Left(AuthFailure(errorType: errorType));
+    }
+    return Right(null);
+  }
+}
