9209facda6790e1328122fc477bdee7896a42dd5
nyel <yhiamdan@gmail.com>
2025-07-10 20:22:22 +0000

    Implement app router, wrapper around Modular

---
+++ lib/libraries/router/modular_navigator_impl.dart
@@ -0,0 +1,21 @@
+// coverage:ignore-file
+import 'package:construculator/libraries/router/interfaces/app_router.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+/// This is the implementation of the app router using Modular.
+class ModularRouterImpl implements AppRouter {
+  @override
+  Future<void> pushNamed(String route, {Object? arguments}) {
+    return Modular.to.pushNamed(route, arguments: arguments);
+  }
+
+  @override
+  void navigate(String route, {Object? arguments}) {
+    Modular.to.navigate(route, arguments: arguments);
+  }
+
+  @override
+  void pop<T extends Object?>([T? result]) {
+    Modular.to.pop();
+  }
+}
