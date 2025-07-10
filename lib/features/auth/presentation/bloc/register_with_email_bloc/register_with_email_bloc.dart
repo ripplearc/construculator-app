5659ba0843d115bbc39835f412c167696acbac4e
nyel <yhiamdan@gmail.com>
2025-07-10 21:58:06 +0000

    Implement register with email bloc

---
+++ lib/features/auth/presentation/bloc/register_with_email_bloc/register_with_email_bloc.dart
@@ -0,0 +1,69 @@
+import 'package:construculator/libraries/auth/data/types/auth_types.dart';
+import 'package:construculator/libraries/errors/failures.dart';
+import 'package:equatable/equatable.dart';
+import 'package:construculator/features/auth/domain/usecases/check_email_availability_usecase.dart';
+import 'package:construculator/features/auth/domain/usecases/send_otp_usecase.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rxdart/rxdart.dart';
+
+part 'register_with_email_event.dart';
+part 'register_with_email_state.dart';
+
+class RegisterWithEmailBloc
+    extends Bloc<RegisterWithEmailEvent, RegisterWithEmailState> {
+  final CheckEmailAvailabilityUseCase _checkEmailAvailabilityUseCase;
+  final SendOtpUseCase _sendOtpUseCase;
+
+  RegisterWithEmailBloc({
+    required CheckEmailAvailabilityUseCase checkEmailAvailabilityUseCase,
+    required SendOtpUseCase sendOtpUseCase,
+  }) : _checkEmailAvailabilityUseCase = checkEmailAvailabilityUseCase,
+       _sendOtpUseCase = sendOtpUseCase,
+       super(RegisterWithEmailInitial()) {
+    on<RegisterWithEmailEmailChanged>(
+      _onEmailChanged,
+      transformer: (events, mapper) {
+        return events
+            .debounceTime(const Duration(milliseconds: 300))
+            .asyncExpand(mapper);
+      },
+    );
+    on<RegisterWithEmailContinuePressed>(_onContinuePressed);
+    on<RegisterWithEmailEditEmail>(_onEditEmail);
+  }
+
+  Future<void> _onEditEmail(
+    RegisterWithEmailEditEmail event,
+    Emitter<RegisterWithEmailState> emit,
+  ) async {
+    emit(RegisterWithEmailEditUserEmail());
+  }
+
+  Future<void> _onEmailChanged(
+    RegisterWithEmailEmailChanged event,
+    Emitter<RegisterWithEmailState> emit,
+  ) async {
+    emit(RegisterWithEmailEmailCheckLoading());
+    final result = await _checkEmailAvailabilityUseCase(event.email);
+    result.fold(
+      (failure) => emit(RegisterWithEmailEmailCheckFailure(failure: failure)),
+      (result) {
+        emit(RegisterWithEmailEmailCheckSuccess(isEmailRegistered: result.data ?? false));
+      },
+    );
+  }
+
+  Future<void> _onContinuePressed(
+    RegisterWithEmailContinuePressed event,
+    Emitter<RegisterWithEmailState> emit,
+  ) async {
+    emit(RegisterWithEmailOtpSendingLoading());
+    final result = await _sendOtpUseCase(event.email, OtpReceiver.email);
+    result.fold(
+      (failure) => emit(RegisterWithEmailOtpSendingFailure(failure: failure)),
+      (_) {
+        emit(RegisterWithEmailOtpSendingSuccess());
+      },
+    );
+  }
+}
