a31f9dd42a13d0c9a305113baedbe91e590797e1
nyel <yhiamdan@gmail.com>
2025-07-11 22:06:20 +0000

    Fix module setup

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -3,7 +3,6 @@ import 'package:construculator/features/dashboard/dashboard_module.dart';
 import 'package:construculator/libraries/auth/auth_library_module.dart';
 import 'package:construculator/libraries/config/config_module.dart';
 import 'package:construculator/app/module_param.dart';
-import 'package:construculator/libraries/supabase/supabase_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
@@ -12,12 +11,11 @@ class AppModule extends Module {
   @override
   List<Module> get imports => [
     ConfigModule(moduleParam),
-    SupabaseModule(moduleParam),
     AuthLibraryModule(moduleParam),
   ];
   @override
   void routes(RouteManager r) {
-    r.module('/', module: DashboardModule());
+    r.module('/', module: DashboardModule(moduleParam));
     r.module('/auth', module: AuthModule(moduleParam));
   }
 }
