607b0bc8232fb5fc265f02f514e5b8d2911d0254
nyel <yhiamdan@gmail.com>
2025-07-11 16:53:12 +0000

    add same password error

--- lib/libraries/supabase/data/supabase_types.dart
+++ lib/libraries/supabase/data/supabase_types.dart
@@ -99,6 +99,9 @@ enum SupabaseAuthErrorCode {
       case 'user_already_exists':
         return SupabaseAuthErrorCode.emailExists;
 
+      case 'same_password':
+        return SupabaseAuthErrorCode.samePassword;
+
       case 'over_request_rate_limit':
       case 'over_email_send_rate_limit':
       case 'over_sms_send_rate_limit':
@@ -131,6 +134,8 @@ enum SupabaseAuthErrorCode {
         return 'over_request_rate_limit';
       case SupabaseAuthErrorCode.timeout:
         return 'request_timeout';
+      case SupabaseAuthErrorCode.samePassword:
+        return 'same_password';
       case SupabaseAuthErrorCode.registrationFailure:
         return 'signup_disabled';
       case SupabaseAuthErrorCode.samePassword:
