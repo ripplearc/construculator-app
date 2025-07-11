f2fd3da57ad5131f02115f6416f9b5dbe28bfe2f
nyel <yhiamdan@gmail.com>
2025-07-11 22:06:20 +0000

    Fix module setup

--- lib/features/dashboard/dashboard_module.dart
+++ lib/features/dashboard/dashboard_module.dart
@@ -1,4 +1,6 @@
+import 'package:construculator/app/module_param.dart';
 import 'package:construculator/features/dashboard/presentation/pages/dashboard_page.dart';
+import 'package:construculator/libraries/auth/auth_library_module.dart';
 import 'package:construculator/libraries/router/guards/auth_guard.dart';
 import 'package:construculator/libraries/router/router_module.dart';
 import 'package:construculator/libraries/router/routes/dashboard_routes.dart';
@@ -6,8 +8,14 @@ import 'package:construculator/libraries/toast/toast_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class DashboardModule extends Module {
+  final ModuleParam moduleParam;
+  DashboardModule(this.moduleParam);
   @override
-  List<Module> get imports => [ToastModule(), RouterModule()];
+  List<Module> get imports => [
+    AuthLibraryModule(moduleParam),
+    ToastModule(),
+    RouterModule(),
+  ];
   @override
   void routes(RouteManager r) {
     r.child(
