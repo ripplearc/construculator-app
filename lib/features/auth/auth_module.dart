0f621386b851c80ebdff170aca3770f6da7d9624
nyel <yhiamdan@gmail.com>
2025-07-11 23:21:36 +0000

    Register blocs as factory

--- lib/features/auth/auth_module.dart
+++ lib/features/auth/auth_module.dart
@@ -186,7 +186,8 @@ class AuthModule extends Module {
     i.add<ForgotPasswordBloc>(
       () => ForgotPasswordBloc(resetPasswordUseCase: i()),
     );
-    i.addSingleton<SetNewPasswordBloc>(
+    
+    i.add<SetNewPasswordBloc>(
       () => SetNewPasswordBloc(setNewPasswordUseCase: i()),
     );
   }
