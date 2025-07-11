ad4b0d6b633d3f13500b098a22a523ab7e178c1c
nyel <yhiamdan@gmail.com>
2025-07-11 19:11:18 +0000

    Fix restack errors

--- lib/features/auth/testing/auth_test_module.dart
+++ lib/features/auth/testing/auth_test_module.dart
@@ -9,6 +9,7 @@ import 'package:construculator/features/auth/domain/usecases/verify_otp_usecase.
 import 'package:construculator/features/auth/domain/usecases/login_usecase.dart';
 import 'package:construculator/features/auth/domain/usecases/set_new_password_usecase.dart';
 import 'package:construculator/features/auth/presentation/bloc/otp_verification_bloc/otp_verification_bloc.dart';
+import 'package:construculator/features/auth/presentation/bloc/register_with_email_bloc/register_with_email_bloc.dart';
 import 'package:construculator/libraries/auth/auth_library_module.dart';
 import 'package:construculator/libraries/config/testing/fake_app_config.dart';
 import 'package:construculator/libraries/config/testing/fake_env_loader.dart';
@@ -46,6 +47,12 @@ class AuthTestModule extends Module {
     i.add<OtpVerificationBloc>(
       () => OtpVerificationBloc(
         verifyOtpUseCase: i(),
+        sendOtpUseCase: i(),
+      ),
+    );
+    i.add<RegisterWithEmailBloc>(
+      () => RegisterWithEmailBloc(
+        checkEmailAvailabilityUseCase: i(),
         sendOtpUseCase: i(),
       ),
     );
