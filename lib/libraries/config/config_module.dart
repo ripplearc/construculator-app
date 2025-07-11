e823c05f426f6a04827d9ce168e329f873be2415
nyel <yhiamdan@gmail.com>
2025-07-11 15:45:38 +0000

    Fix restack errors

--- lib/libraries/config/config_module.dart
+++ lib/libraries/config/config_module.dart
@@ -1,13 +1,14 @@
-import 'package:construculator/libraries/config/app_config_impl.dart';
-import 'package:construculator/libraries/config/env_loader_impl.dart';
 import 'package:construculator/libraries/config/interfaces/config.dart';
 import 'package:construculator/libraries/config/interfaces/env_loader.dart';
+import 'package:construculator/app/module_param.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class ConfigModule extends Module {
+  final ModuleParam moduleParam;
+  ConfigModule(this.moduleParam);
   @override
   void exportedBinds(Injector i) {
-    i.addSingleton<EnvLoader>(() => EnvLoaderImpl());
-    i.addSingleton<Config>(() => AppConfigImpl(envLoader: i()));
+    i.addSingleton<EnvLoader>(() => moduleParam.envLoader);
+    i.addSingleton<Config>(() => moduleParam.config);
   }
 }
--- lib/libraries/supabase/supabase_module.dart
+++ lib/libraries/supabase/supabase_module.dart
@@ -1,13 +1,15 @@
 import 'package:construculator/libraries/config/config_module.dart';
+import 'package:construculator/app/module_param.dart';
 import 'package:construculator/libraries/supabase/interfaces/supabase_wrapper.dart';
-import 'package:construculator/libraries/supabase/supabase_wrapper_impl.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class SupabaseModule extends Module {
+  final ModuleParam moduleParam;
+  SupabaseModule(this.moduleParam);
   @override
-  List<Module> get imports => [ConfigModule()];
+  List<Module> get imports => [ConfigModule(moduleParam)];
   @override
   void exportedBinds(Injector i) {
-    i.addLazySingleton<SupabaseWrapper>(() => SupabaseWrapperImpl(envLoader: i()));
+    i.addLazySingleton<SupabaseWrapper>(() => moduleParam.supabaseWrapper);
   }
 }
