0829caf79ce80d6619a3b9fd49944200db24440e
nyel <yhiamdan@gmail.com>
2025-07-11 15:45:38 +0000

    Fix restack errors

---
+++ lib/features/auth/testing/auth_test_module.dart
@@ -0,0 +1,55 @@
+import 'package:construculator/app/module_param.dart';
+import 'package:construculator/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:construculator/features/auth/data/datasources/auth_remote_data_source_impl.dart';
+import 'package:construculator/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:construculator/features/auth/domain/repositories/auth_repository.dart';
+import 'package:construculator/features/auth/domain/usecases/check_email_availability_usecase.dart';
+import 'package:construculator/features/auth/domain/usecases/create_account_usecase.dart';
+import 'package:construculator/features/auth/domain/usecases/get_professional_roles_usecase.dart';
+import 'package:construculator/features/auth/domain/usecases/reset_password_usecase.dart';
+import 'package:construculator/features/auth/domain/usecases/send_otp_usecase.dart';
+import 'package:construculator/features/auth/domain/usecases/verify_otp_usecase.dart';
+import 'package:construculator/features/auth/domain/usecases/login_usecase.dart';
+import 'package:construculator/features/auth/domain/usecases/set_new_password_usecase.dart';
+import 'package:construculator/libraries/auth/auth_library_module.dart';
+import 'package:construculator/libraries/config/testing/fake_app_config.dart';
+import 'package:construculator/libraries/config/testing/fake_env_loader.dart';
+import 'package:construculator/libraries/router/testing/router_test_module.dart';
+import 'package:construculator/libraries/supabase/testing/fake_supabase_wrapper.dart';
+import 'package:construculator/libraries/toast/toast_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AuthTestModule extends Module {
+  @override
+  List<Module> get imports => [
+    AuthLibraryModule(
+      ModuleParam(
+        envLoader: FakeEnvLoader(),
+        config: FakeAppConfig(),
+        supabaseWrapper: FakeSupabaseWrapper(),
+      ),
+    ),
+    ToastModule(),
+    RouterTestModule(),
+  ];
+
+  @override
+  void binds(Injector i) {
+    i.addSingleton<AuthRemoteDataSource>(
+      () => AuthRemoteDataSourceImpl(supabaseWrapper: i()),
+    );
+    i.add<AuthRepository>(() => AuthRepositoryImpl(remoteDataSource: i()));
+    i.addLazySingleton<ResetPasswordUseCase>(() => ResetPasswordUseCase(i()));
+    i.addLazySingleton<GetProfessionalRolesUseCase>(
+      () => GetProfessionalRolesUseCase(i()),
+    );
+    i.addLazySingleton<CheckEmailAvailabilityUseCase>(
+      () => CheckEmailAvailabilityUseCase(i()),
+    );
+    i.addLazySingleton<CreateAccountUseCase>(() => CreateAccountUseCase(i()));
+    i.addLazySingleton<SendOtpUseCase>(() => SendOtpUseCase(i()));
+    i.addLazySingleton<VerifyOtpUseCase>(() => VerifyOtpUseCase(i()));
+    i.addLazySingleton<LoginUseCase>(() => LoginUseCase(i()));
+    i.addLazySingleton<SetNewPasswordUseCase>(() => SetNewPasswordUseCase(i()));
+  }
+}
