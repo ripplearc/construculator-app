b9fb69228bef7eed05988f698f7dc0c21b333872
nyel <yhiamdan@gmail.com>
2025-07-11 22:50:14 +0000

    Fix router issues

--- lib/features/auth/presentation/pages/forgot_password_page.dart
+++ lib/features/auth/presentation/pages/forgot_password_page.dart
@@ -170,7 +170,7 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                 otp = state.otp;
               }
               if (state is OtpVerificationSuccess) {
-                _router.navigate(fullSetNewPasswordRoute, arguments: email);
+                _router.pushNamed(fullSetNewPasswordRoute, arguments: email);
               }
               if (state is OtpVerificationFailure) {
                 _handleFailure(state.failure);
