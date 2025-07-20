160082b272624ef967f616eaac7d49f868e79178
nyel <yhiamdan@gmail.com>
2025-07-20 16:33:15 +0000

    Add comments for app module

--- lib/app/module_param.dart
+++ lib/app/module_param.dart
@@ -2,7 +2,21 @@ import 'package:construculator/libraries/config/interfaces/config.dart';
 import 'package:construculator/libraries/config/interfaces/env_loader.dart';
 import 'package:construculator/libraries/supabase/interfaces/supabase_wrapper.dart';
 
-/// This class is used to pass parameters to the app module.
+/// The app module requires that the supabase client is initialized before the app module starts, this is for the purposes of route guard checks.
+/// This can not be done in the constructor of the app module, and modules do not have any special init functions for this purpose.
+/// Supabase client depends on [Config] and [EnvLoader] for initialization.
+/// Both [Config] and [EnvLoader] are passed to the app module as parameters to ensure the same instance used for the supabase client 
+/// is registered and used across the app.
+/// Example usage:
+/// ```dart
+/// final Environment env = _getEnvironmentFromString(envName);
+/// final envLoader = EnvLoaderImpl();
+/// final config = AppConfigImpl(envLoader: envLoader);
+/// await config.initialize(env);
+/// final wrapper = SupabaseWrapperImpl(envLoader: envLoader);
+/// await wrapper.initialize();
+/// final params = ModuleParam(config: config, envLoader: envLoader, supabaseWrapper: wrapper);
+/// runApp(ModularApp(module: AppModule(params), child: const AppWidget()));
 class ModuleParam {
   /// The environment loader, required by [Config] for initialization.
   final EnvLoader envLoader;
