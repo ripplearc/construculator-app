c10ea6867ff91daeb71f847e3ed8e5afa6fe4679
nyel <yhiamdan@gmail.com>
2025-07-25 13:34:48 +0000

    fix restack errors

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,11 +1,21 @@
+import 'package:construculator/features/auth/auth_module.dart';
+import 'package:construculator/libraries/auth/auth_library_module.dart';
+import 'package:construculator/libraries/config/config_module.dart';
+import 'package:construculator/app/module_param.dart';
+import 'package:construculator/libraries/supabase/supabase_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
 class AppModule extends Module {
+  final ModuleParam moduleParam;
+  AppModule(this.moduleParam);
   @override
-  List<Module> get imports => [];
-
-
+  List<Module> get imports => [
+    ConfigModule(moduleParam),
+    SupabaseModule(moduleParam),
+    AuthLibraryModule(moduleParam),
+  ];
   @override
   void routes(RouteManager r) {
-   // setup routes here
+    r.module('/auth', module: AuthModule(moduleParam));
   }
 }
