26b36b31b1770672b2c8d342289c2a3ad3607f72
nyel <yhiamdan@gmail.com>
2025-08-05 01:43:33 +0000

    remove toast and success modal

--- lib/libraries/toast/toast_module.dart
+++
@@ -1,9 +0,0 @@
-import 'package:construculator/libraries/toast/toast.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-
-class ToastModule extends Module {
-  @override
-  void exportedBinds(Injector i) {
-    i.addSingleton<CToast>(CToast.new);
-  }
-}
