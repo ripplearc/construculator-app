98e956369da6f013fe3b418b37580c9bcb07d93b
nyel <yhiamdan@gmail.com>
2025-08-07 16:27:52 +0000

    fix formats

--- lib/features/auth/presentation/bloc/create_account_bloc/create_account_bloc.dart
+++ lib/features/auth/presentation/bloc/create_account_bloc/create_account_bloc.dart
@@ -2,6 +2,7 @@ import 'package:construculator/features/auth/domain/usecases/create_account_usec
 import 'package:construculator/features/auth/domain/usecases/get_professional_roles_usecase.dart';
 import 'package:construculator/features/auth/domain/usecases/params/create_account_usecase_params.dart';
 import 'package:construculator/features/auth/domain/usecases/send_otp_usecase.dart';
+import 'package:construculator/libraries/auth/data/models/professional_role.dart';
 import 'package:construculator/libraries/auth/data/types/auth_types.dart';
 import 'package:construculator/libraries/errors/failures.dart';
 import 'package:equatable/equatable.dart';
@@ -37,7 +38,7 @@ class CreateAccountBloc extends Bloc<CreateAccountEvent, CreateAccountState> {
     emit(CreateAccountEditContactSuccess());
   }
 
-   Future<void> _onOtpVerified(
+  Future<void> _onOtpVerified(
     CreateAccountOtpVerified event,
     Emitter<CreateAccountState> emit,
   ) async {
@@ -51,47 +52,50 @@ class CreateAccountBloc extends Bloc<CreateAccountEvent, CreateAccountState> {
     emit(CreateAccountGetProfessionalRolesLoading());
     final result = await _getProfessionalRolesUseCase();
     result.fold(
-      (failure) => emit(
-        CreateAccountGetProfessionalRolesFailure(failure: failure),
+      (failure) =>
+          emit(CreateAccountGetProfessionalRolesFailure(failure: failure)),
+      (roles) => emit(
+        CreateAccountGetProfessionalRolesSuccess(professionalRolesList: roles),
       ),
-      (roles) => emit(CreateAccountGetProfessionalRolesSuccess(professionalRolesList: roles)),
     );
   }
-    Future<void> _onSendOtp(
+
+  Future<void> _onSendOtp(
     CreateAccountSendOtp event,
     Emitter<CreateAccountState> emit,
   ) async {
     emit(CreateAccountOtpSending());
     // verification is only available for email during phone registration and for phone
     // during email registration
-    final receiver = event.isEmailRegistration ? OtpReceiver.phone : OtpReceiver.email;
-    final result = await _sendOtpUseCase(event.address,receiver);
+    final receiver = event.isEmailRegistration
+        ? OtpReceiver.phone
+        : OtpReceiver.email;
+    final result = await _sendOtpUseCase(event.address, receiver);
     result.fold(
-      (failure) => emit(
-        CreateAccountOtpSendingFailure(failure: failure),
-      ),
+      (failure) => emit(CreateAccountOtpSendingFailure(failure: failure)),
       (roles) => emit(CreateAccountOtpSendingSuccess(contact: event.address)),
     );
   }
+
   Future<void> _onSubmitted(
     CreateAccountSubmitted event,
-    Emitter<CreateAccountState> emit, 
+    Emitter<CreateAccountState> emit,
   ) async {
     emit(CreateAccountLoading());
-    final result = await _createAccountUseCase(CreateAccountUseCaseParams(
-      email: event.email,
-      firstName: event.firstName,
-      lastName: event.lastName,
-      password: event.password,
-      professionalRole: event.role,
-      phone: event.mobileNumber,
-      countryCode: event.phonePrefix,
-    ));
-    result.fold(
-      (failure) => emit(
-        CreateAccountFailure(failure: failure),
+    final result = await _createAccountUseCase(
+      CreateAccountUseCaseParams(
+        email: event.email,
+        firstName: event.firstName,
+        lastName: event.lastName,
+        password: event.password,
+        professionalRole: event.role,
+        phone: event.mobileNumber,
+        countryCode: event.phonePrefix,
       ),
+    );
+    result.fold(
+      (failure) => emit(CreateAccountFailure(failure: failure)),
       (roles) => emit(CreateAccountSuccess()),
     );
- }
+  }
 }
