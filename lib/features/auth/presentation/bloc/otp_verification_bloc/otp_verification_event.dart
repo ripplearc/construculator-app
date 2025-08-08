3bd613d2cde5b1bf07332eb15bcb6aa690954a0f
nyel <yhiamdan@gmail.com>
2025-08-08 13:56:00 +0000

    Add comments to otp verification bloc

--- lib/features/auth/presentation/bloc/otp_verification_bloc/otp_verification_event.dart
+++ lib/features/auth/presentation/bloc/otp_verification_bloc/otp_verification_event.dart
@@ -1,15 +1,24 @@
 // coverage:ignore-file
 part of 'otp_verification_bloc.dart';
 
+/// This is the event class for the OTP verification bloc.
+/// It is used to type the events for the OTP verification bloc.
 abstract class OtpVerificationEvent extends Equatable {
+  /// This is the constructor for the OTP verification event.
   const OtpVerificationEvent();
 
+  /// This is the method to get the properties of the OTP verification event.
   @override
   List<Object> get props => [];
 }
 
+/// This is the event class for the OTP verification submitted event.
+/// It is triggered when the user submits the OTP verification.
 class OtpVerificationSubmitted extends OtpVerificationEvent {
+  /// The contact number to verify.
   final String contact;
+
+  /// The OTP for verification.
   final String otp;
 
   const OtpVerificationSubmitted({required this.contact, required this.otp});
@@ -18,7 +27,10 @@ class OtpVerificationSubmitted extends OtpVerificationEvent {
   List<Object> get props => [contact, otp];
 }
 
+/// This is the event class for the OTP verification resend request event.
+/// It is triggered when the user requests to resend the OTP verification.
 class OtpVerificationResendRequested extends OtpVerificationEvent {
+  /// The contact number to send the OTP to.
   final String contact;
 
   const OtpVerificationResendRequested({required this.contact});
@@ -27,7 +39,10 @@ class OtpVerificationResendRequested extends OtpVerificationEvent {
   List<Object> get props => [contact];
 } 
 
+/// This is the event class for the OTP verification OTP changed event.
+/// It is triggered when the OTP input field is being edited.
 class OtpVerificationOtpChanged extends OtpVerificationEvent {
+  /// The current OTP value.
   final String otp;
 
   const OtpVerificationOtpChanged({required this.otp});
--- lib/features/auth/presentation/bloc/otp_verification_bloc/otp_verification_state.dart
+++ lib/features/auth/presentation/bloc/otp_verification_bloc/otp_verification_state.dart
@@ -1,31 +1,31 @@
 // coverage:ignore-file
 part of 'otp_verification_bloc.dart';
 
+/// This is the state class for the OTP verification bloc.
+/// It is used to type the states for the OTP verification bloc.
 abstract class OtpVerificationState extends Equatable {
+  /// This is the constructor for the OTP verification state.
   const OtpVerificationState();
 
+  /// This is the method to get the properties of the OTP verification state.
   @override
   List<Object> get props => [];
 }
 
+/// This is the state class for the OTP verification initial state.
+/// It is used to represent the initial state of the OTP verification bloc.
 class OtpVerificationInitial extends OtpVerificationState {}
 
+/// This is the state class for the OTP verification loading state.
+/// It is used to represent the loading state of the OTP verification
+///  bloc when a request is made to verify the OTP.
 class OtpVerificationLoading extends OtpVerificationState {}
 
-class OtpVerificationOtpResent extends OtpVerificationState {}
-
-class OtpVerificationResendLoading extends OtpVerificationState{}
-
-class OtpVerificationResendFailure extends OtpVerificationState {
-  final Failure failure;
-
-  const OtpVerificationResendFailure(this.failure);
-
-  @override
-  List<Object> get props => [failure];
-}
-
+/// This is the state class for the OTP verification success state.
+/// It is used to represent the success state of the OTP verification bloc 
+/// when the OTP is verified successfully.
 class OtpVerificationSuccess extends OtpVerificationState {
+  /// The email address of the user.
   final String email;
   const OtpVerificationSuccess({required this.email});
 
@@ -33,7 +33,11 @@ class OtpVerificationSuccess extends OtpVerificationState {
   List<Object> get props => [email];
 }
 
+/// This is the state class for the OTP verification failure state.
+/// It is used to represent the failure state of the OTP verification bloc
+///  when the otp verification fails.
 class OtpVerificationFailure extends OtpVerificationState {
+  /// The failure reason.
   final Failure failure;
 
   const OtpVerificationFailure(this.failure);
@@ -42,7 +46,35 @@ class OtpVerificationFailure extends OtpVerificationState {
   List<Object> get props => [failure];
 } 
 
+
+/// This is the state class for the OTP verification resend loading state.
+/// It is used to represent the resend loading state of the OTP verification bloc
+/// when a request is made to resend the OTP.
+class OtpVerificationResendLoading extends OtpVerificationState{}
+
+/// This is the state class for the OTP verification OTP resent state.
+/// It is used to represent the OTP resent state of the OTP verification bloc
+/// when the OTP is resent successfully.
+class OtpVerificationOtpResent extends OtpVerificationState {}
+
+/// This is the state class for the OTP verification resend failure state.
+/// It is used to represent the resend failure state of the OTP verification bloc
+/// when the OTP resend fails.
+class OtpVerificationResendFailure extends OtpVerificationState {
+  /// The failure reason.
+  final Failure failure;
+
+  const OtpVerificationResendFailure(this.failure);
+
+  @override
+  List<Object> get props => [failure];
+}
+
+/// This is the state class for the OTP verification OTP change updated state.
+/// It is used to represent the OTP change updated state of the OTP verification bloc
+/// when the OTP input field is being edited.
 class OtpVerificationOtpChangeUpdated extends OtpVerificationState {
+  /// The current OTP value.
   final String otp;
   const OtpVerificationOtpChangeUpdated({required this.otp});
 
