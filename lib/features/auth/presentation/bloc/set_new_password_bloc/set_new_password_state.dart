5e5ecd3b4b90d9a221b2ab7b26b61ec688521250
nyel <yhiamdan@gmail.com>
2025-08-14 18:51:34 +0000

    refactor bloc

--- lib/features/auth/presentation/bloc/set_new_password_bloc/set_new_password_state.dart
+++ lib/features/auth/presentation/bloc/set_new_password_bloc/set_new_password_state.dart
@@ -1,6 +1,12 @@
 // coverage:ignore-file
 part of 'set_new_password_bloc.dart';
 
+/// Enum for form fields that can be validated
+enum SetNewPasswordFormField {
+  password,
+  passwordConfirmation,
+}
+
 abstract class SetNewPasswordState extends Equatable {}
 
 class SetNewPasswordInitial extends SetNewPasswordState {
@@ -24,4 +30,23 @@ class SetNewPasswordFailure extends SetNewPasswordState {
   
   @override
   List<Object?> get props => [failure];
+}
+
+/// State for password validation results
+class SetNewPasswordPasswordValidated extends SetNewPasswordState {
+  /// The field that was validated
+  final SetNewPasswordFormField field;
+  /// Whether the field is valid
+  final bool isValid;
+  /// The validation error type, null if field is valid
+  final AuthErrorType? validator;
+
+  SetNewPasswordPasswordValidated({
+    required this.field,
+    required this.isValid,
+    this.validator,
+  });
+
+  @override
+  List<Object?> get props => [field, isValid, validator];
 }
