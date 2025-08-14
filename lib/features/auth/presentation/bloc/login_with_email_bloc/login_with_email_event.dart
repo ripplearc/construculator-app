77068cde55235ce5d0d5913de1cb56ac6a09cb62
nyel <yhiamdan@gmail.com>
2025-08-14 15:09:51 +0000

    add bloc comments

--- lib/features/auth/presentation/bloc/login_with_email_bloc/login_with_email_event.dart
+++ lib/features/auth/presentation/bloc/login_with_email_bloc/login_with_email_event.dart
@@ -1,26 +1,23 @@
 // coverage:ignore-file
 part of 'login_with_email_bloc.dart';
 
+/// Abstract class for login with email events
 abstract class LoginWithEmailEvent extends Equatable {
+  /// Constructor for login with email events
   const LoginWithEmailEvent();
 
+  /// List of properties that will be used to compare events
   @override
   List<Object> get props => [];
 }
 
+/// Event triggered when email is changed, which triggers the validation of the email
 class LoginEmailChanged extends LoginWithEmailEvent {
   const LoginEmailChanged(this.email);
 
+  /// The email entered by the user
   final String email;
 
   @override
   List<Object> get props => [email];
 }
-
-class LoginEmailSubmitted extends LoginWithEmailEvent {
-  final String email;
-  const LoginEmailSubmitted(this.email);
-
-  @override
-  List<Object> get props => [email];
-} 
