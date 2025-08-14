f23048bf7a2d41a3bb57379ce2bca05aa42eb844
hyperdanyel <danyel.nizy92@gmail.com>
2025-08-14 08:19:03 +0000

    CA-30 - Add professional roles data access and repository implementation (#53)
    * Implement auth data model and repo
    
    * Implement tests for auth repository
    
    * Fix restack errors
    
    * Refactor add as factory instead of singleton
    
    * refactor
    
    * refactor
    
    * remove toasts
    
    * move professional roles to auth/lib
    
    * fix restack errors
    
    ---------
    
    Co-authored-by: nyel <yhiamdan@gmail.com>

--- lib/libraries/config/config_module.dart
+++ lib/libraries/config/config_module.dart
@@ -1,13 +1,14 @@
-import 'package:construculator/libraries/config/app_config_impl.dart';
-import 'package:construculator/libraries/config/env_loader_impl.dart';
 import 'package:construculator/libraries/config/interfaces/config.dart';
 import 'package:construculator/libraries/config/interfaces/env_loader.dart';
+import 'package:construculator/app/app_bootstrap.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class ConfigModule extends Module {
+  final AppBootstrap appBootstrap;
+  ConfigModule(this.appBootstrap);
   @override
   void exportedBinds(Injector i) {
-    i.addSingleton<EnvLoader>(() => EnvLoaderImpl());
-    i.addSingleton<Config>(() => AppConfigImpl(envLoader: i()));
+    i.addSingleton<EnvLoader>(() => appBootstrap.envLoader);
+    i.addSingleton<Config>(() => appBootstrap.config);
   }
 }
--- lib/libraries/supabase/supabase_module.dart
+++ lib/libraries/supabase/supabase_module.dart
@@ -1,13 +1,15 @@
 import 'package:construculator/libraries/config/config_module.dart';
+import 'package:construculator/app/app_bootstrap.dart';
 import 'package:construculator/libraries/supabase/interfaces/supabase_wrapper.dart';
-import 'package:construculator/libraries/supabase/supabase_wrapper_impl.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class SupabaseModule extends Module {
+  final AppBootstrap appBootstrap;
+  SupabaseModule(this.appBootstrap);
   @override
-  List<Module> get imports => [ConfigModule()];
+  List<Module> get imports => [ConfigModule(appBootstrap)];
   @override
   void exportedBinds(Injector i) {
-    i.addLazySingleton<SupabaseWrapper>(() => SupabaseWrapperImpl(envLoader: i()));
+    i.addLazySingleton<SupabaseWrapper>(() => appBootstrap.supabaseWrapper);
   }
 }
