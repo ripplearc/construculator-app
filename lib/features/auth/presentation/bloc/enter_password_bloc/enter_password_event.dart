276601a12dcf7875207e2cf7c16744c851db3af5
nyel <yhiamdan@gmail.com>
2025-08-14 15:12:53 +0000

    add bloc comments

--- lib/features/auth/presentation/bloc/enter_password_bloc/enter_password_event.dart
+++ lib/features/auth/presentation/bloc/enter_password_bloc/enter_password_event.dart
@@ -1,12 +1,25 @@
 // coverage:ignore-file
 part of 'enter_password_bloc.dart';
 
-abstract class EnterPasswordEvent extends Equatable {}
+/// Abstract class for enter password events
+abstract class EnterPasswordEvent extends Equatable {
+  /// Constructor for enter password events
+  const EnterPasswordEvent();
 
+  /// List of properties that will be used to compare events
+  @override
+  List<Object> get props => [];
+}
+
+/// Event triggered when the user submits the password
 class EnterPasswordSubmitted extends EnterPasswordEvent {
+  /// The email of the user
   final String email;
+  /// The password entered by the user
   final String password;
-  EnterPasswordSubmitted({required this.email, required this.password});
+
+  const EnterPasswordSubmitted({required this.email, required this.password});
+
   @override
   List<Object> get props => [email, password];
 }
