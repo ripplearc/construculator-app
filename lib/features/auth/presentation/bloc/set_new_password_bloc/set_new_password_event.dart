de1ed3543e94830c06a9d2cb4e677e3e9809b13b
nyel <yhiamdan@gmail.com>
2025-08-14 18:54:36 +0000

    refactor bloc

--- lib/features/auth/presentation/bloc/set_new_password_bloc/set_new_password_event.dart
+++ lib/features/auth/presentation/bloc/set_new_password_bloc/set_new_password_event.dart
@@ -1,21 +1,32 @@
 // coverage:ignore-file
 part of 'set_new_password_bloc.dart';
 
+/// Abstract class for set new password events
 abstract class SetNewPasswordEvent extends Equatable {
+  /// Constructor for set new password events
   const SetNewPasswordEvent();
+
+  /// List of properties that will be used to compare events
   @override
   List<Object> get props => [];
 }
 
+/// Event triggered when the user submits the new password
 class SetNewPasswordSubmitted extends SetNewPasswordEvent {
+  /// The email of the user
   final String email;
+  /// The new password entered by the user
   final String password;
   const SetNewPasswordSubmitted({required this.email, required this.password});
 }
 
+/// Event triggered as user types in the new password
 class SetNewPasswordPasswordValidationRequested extends SetNewPasswordEvent {
+  /// The field that was validated
   final SetNewPasswordFormField field;
+  /// The value of the field that was validated
   final String value;
+  /// The value of the password to confirm against the password value
   final String? passwordValue;
 
   const SetNewPasswordPasswordValidationRequested({
--- lib/features/auth/presentation/bloc/set_new_password_bloc/set_new_password_state.dart
+++ lib/features/auth/presentation/bloc/set_new_password_bloc/set_new_password_state.dart
@@ -7,26 +7,39 @@ enum SetNewPasswordFormField {
   passwordConfirmation,
 }
 
-abstract class SetNewPasswordState extends Equatable {}
+/// Abstract class for set new password states
+abstract class SetNewPasswordState extends Equatable {
+  /// Constructor for set new password states
+  const SetNewPasswordState();
 
+  /// List of properties that will be used to compare states
+  @override
+  List<Object?> get props => [];
+}
+
+/// The initial bloc state, typically when the set_new_password page loads.
 class SetNewPasswordInitial extends SetNewPasswordState {
   @override
   List<Object?> get props => [];
 }
 
+/// State when the set new password process is loading, after the user presses the continue button
 class SetNewPasswordLoading extends SetNewPasswordState {
   @override
   List<Object?> get props => [];
 }
 
+/// State when the set new password process is successful
 class SetNewPasswordSuccess extends SetNewPasswordState {
   @override
   List<Object?> get props => [];
 }
 
+/// State when the set new password process fails
 class SetNewPasswordFailure extends SetNewPasswordState {
+  /// The failure that occurred during the set new password process
   final Failure failure;
-  SetNewPasswordFailure({required this.failure});
+  const SetNewPasswordFailure({required this.failure});
   
   @override
   List<Object?> get props => [failure];
@@ -41,7 +54,7 @@ class SetNewPasswordPasswordValidated extends SetNewPasswordState {
   /// The validation error type, null if field is valid
   final AuthErrorType? validator;
 
-  SetNewPasswordPasswordValidated({
+  const SetNewPasswordPasswordValidated({
     required this.field,
     required this.isValid,
     this.validator,
