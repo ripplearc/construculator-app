928ef6bf00c06da56b8e042af0c791c512f827a5
nyel <yhiamdan@gmail.com>
2025-08-14 15:10:54 +0000

    add bloc comments

--- lib/features/auth/presentation/bloc/login_with_email_bloc/login_with_email_state.dart
+++ lib/features/auth/presentation/bloc/login_with_email_bloc/login_with_email_state.dart
@@ -1,44 +1,66 @@
 // coverage:ignore-file
 part of 'login_with_email_bloc.dart';
 
-abstract class LoginWithEmailState extends Equatable {}
+/// Abstract class for login with email states
+abstract class LoginWithEmailState extends Equatable {
+  /// Constructor for login with email states
+  const LoginWithEmailState();
 
+  /// List of properties that will be used to compare states
+  @override
+  List<Object?> get props => [];
+}
+
+/// The initial bloc state, typically when the login_with_email page loads.
 class LoginWithEmailInitial extends LoginWithEmailState {
   @override
   List<Object?> get props => [];
 }
+
+/// State when the login process is loading, after the user enters password 
+/// and continue button is pressed
 class LoginWithEmailLoading extends LoginWithEmailState {
   @override
   List<Object?> get props => [];
 }
+
+/// State when the login process is successful
 class LoginWithEmailSuccess extends LoginWithEmailState {
   @override
   List<Object?> get props => [];
 }
+
+/// State when the login process fails
 class LoginWithEmailFailure extends LoginWithEmailState {
+  /// The failure that occurred during the login process
   final Failure failure;
-  LoginWithEmailFailure({required this.failure});
+  const LoginWithEmailFailure({required this.failure});
 
   @override
   List<Object?> get props => [failure];
 }
 
+/// State when the email availability is being checked
 class LoginWithEmailAvailabilityLoading extends LoginWithEmailState {
   @override
   List<Object?> get props => [];
 }
 
-class LoginWithEmailAvailabilitySuccess extends LoginWithEmailState {
+/// State when the email availability is checked successfully
+class LoginWithEmailAvailabilityLoaded extends LoginWithEmailState {
+  /// Whether the email is registered
   final bool isEmailRegistered;
-  LoginWithEmailAvailabilitySuccess({required this.isEmailRegistered});
+  const LoginWithEmailAvailabilityLoaded({required this.isEmailRegistered});
 
   @override
   List<Object?> get props => [isEmailRegistered];
 }
 
+/// State when the email availability check fails
 class LoginWithEmailAvailabilityFailure extends LoginWithEmailState {
+  /// The failure that occurred during the email availability check
   final Failure failure;
-  LoginWithEmailAvailabilityFailure({required this.failure});
+  const LoginWithEmailAvailabilityFailure({required this.failure});
 
   @override
   List<Object?> get props => [failure];
