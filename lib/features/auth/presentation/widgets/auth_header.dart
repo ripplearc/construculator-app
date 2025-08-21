f563ce828552939074becbfb8c36d31d1fe483ac
nyel <yhiamdan@gmail.com>
2025-08-21 16:19:46 +0000

    add auth header

---
+++ lib/features/auth/presentation/widgets/auth_header.dart
@@ -0,0 +1,27 @@
+import 'package:core_ui/core_ui.dart';
+import 'package:flutter/widgets.dart';
+
+class AuthHeader extends StatelessWidget {
+  final String title;
+  final String description;
+  const AuthHeader({super.key, required this.title, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: CoreTypography.headlineLargeSemiBold(),
+        ),
+        const SizedBox(height: CoreSpacing.space2),
+        Text(
+          description,
+          style: CoreTypography.bodyLargeRegular(),
+        ),
+      ],
+    );
+  }
+}
