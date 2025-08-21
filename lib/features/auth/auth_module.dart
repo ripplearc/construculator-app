94bcd562fd8510361f5c4a0e402890f337c6b135
nyel <yhiamdan@gmail.com>
2025-08-21 09:56:13 +0000

    fix restack errors

--- lib/features/auth/auth_module.dart
+++ lib/features/auth/auth_module.dart
@@ -148,7 +148,7 @@ void _registerDependencies(Injector i) {
     () => LoginWithEmailBloc(checkEmailAvailabilityUseCase: i()),
   );
   i.add<EnterPasswordBloc>(() => EnterPasswordBloc(loginUseCase: i()));
-  i.addSingleton<ForgotPasswordBloc>(
+  i.add<ForgotPasswordBloc>(
     () => ForgotPasswordBloc(resetPasswordUseCase: i()),
   );
 }
