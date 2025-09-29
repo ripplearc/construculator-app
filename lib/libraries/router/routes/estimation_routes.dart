0a27eb218883659b088f61da7a2c4bf85ccadc2c
Simon Dereje <simondereje1@gmail.com>
2025-09-29 15:25:25 +0300

    chore: remove unnecessary comments from estimation_routes.dart

--- lib/libraries/router/routes/estimation_routes.dart
+++ lib/libraries/router/routes/estimation_routes.dart
@@ -1,9 +1,4 @@
-// base route
 const String estimationBaseRoute = '/estimation';
-
-// route prefixes - used in declaration of routes
 const String estimationLandingRoute = '/landing';
-
-// full routes - used in navigation
 const String fullEstimationLandingRoute =
     '$estimationBaseRoute$estimationLandingRoute';
