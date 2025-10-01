08978fcc0b1af402a73849ef9e0548fbe2220a53
Simon Dereje <simondereje1@gmail.com>
2025-10-01 16:56:01 +0300

    feat: add project settings module and get project usecase

---
+++ lib/features/project/project_module.dart
@@ -0,0 +1,26 @@
+import 'package:construculator/app/app_bootstrap.dart';
+import 'package:construculator/libraries/project/data/repositories/project_repository_impl.dart';
+import 'package:construculator/libraries/project/domain/repositories/project_repository.dart';
+import 'package:construculator/features/project/domain/usecases/get_project_usecase.dart';
+import 'package:construculator/libraries/time/clock_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class ProjectModule extends Module {
+  final AppBootstrap appBootstrap;
+  ProjectModule(this.appBootstrap);
+
+  @override
+  List<Module> get imports => [ClockModule()];
+
+  @override
+  void routes(RouteManager r) {}
+
+  @override
+  void binds(Injector i) => _registerDependencies(i);
+}
+
+void _registerDependencies(Injector i) {
+  i.addLazySingleton<ProjectRepository>(() => ProjectRepositoryImpl());
+
+  i.addLazySingleton<GetProjectUseCase>(() => GetProjectUseCase(i()));
+}
