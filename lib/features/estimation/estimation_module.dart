f5aa6d88bf0deedc554a2162806b174f95d37fa8
Simon Dereje <134320950+SimonDerejeW@users.noreply.github.com>
2025-10-14 16:26:41 +0300

    CA-73 feat: add cost estimation modules (#91)
    This PR primarily introduces the cost estimation module, along with the route file and module route tests.
    
    ### Key Changes
    - Created cost estimation module and the relevant test file.
    - Created a route file for the cost estimation routes.

---
+++ lib/features/estimation/estimation_module.dart
@@ -0,0 +1,38 @@
+import 'package:construculator/app/app_bootstrap.dart';
+import 'package:construculator/libraries/auth/auth_library_module.dart';
+import 'package:construculator/libraries/router/guards/auth_guard.dart';
+import 'package:construculator/libraries/router/router_module.dart';
+import 'package:construculator/libraries/router/routes/estimation_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class RouteDefinition {
+  final String route;
+  final WidgetBuilder widget;
+  final List<RouteGuard> guards;
+
+  RouteDefinition(this.route, this.widget, this.guards);
+}
+
+class EstimationModule extends Module {
+  final AppBootstrap appBootstrap;
+  EstimationModule(this.appBootstrap);
+
+  final List<RouteDefinition> _routeDefinitions = [
+    RouteDefinition(estimationLandingRoute, (context) => Container(), [
+      AuthGuard(),
+    ]),
+  ];
+
+  List<RouteDefinition> get routeDefinitions => _routeDefinitions;
+
+  @override
+  List<Module> get imports => [AuthLibraryModule(appBootstrap), RouterModule()];
+
+  @override
+  void routes(RouteManager r) {
+    for (final routeDef in _routeDefinitions) {
+      r.child(routeDef.route, guards: routeDef.guards, child: routeDef.widget);
+    }
+  }
+}
---
+++ lib/libraries/router/routes/estimation_routes.dart
@@ -0,0 +1,4 @@
+const String estimationBaseRoute = '/estimation';
+const String estimationLandingRoute = '/landing';
+const String fullEstimationLandingRoute =
+    '$estimationBaseRoute$estimationLandingRoute';
