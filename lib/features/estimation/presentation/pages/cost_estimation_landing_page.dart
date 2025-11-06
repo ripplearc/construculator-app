439c82401e39f2ee4209f2e827b29ae72d0f8b34
Simon Dereje <simondereje1@gmail.com>
2025-11-06 04:53:50 +0300

    feat: wire add estimation button to bloc

--- lib/features/estimation/presentation/pages/cost_estimation_landing_page.dart
+++ lib/features/estimation/presentation/pages/cost_estimation_landing_page.dart
@@ -1,11 +1,14 @@
 import 'package:construculator/features/estimation/domain/entities/cost_estimate_entity.dart';
 import 'package:construculator/features/estimation/presentation/bloc/cost_estimation_list_bloc/cost_estimation_list_bloc.dart';
+import 'package:construculator/features/estimation/presentation/bloc/add_cost_estimation_bloc/add_cost_estimation_bloc.dart';
 import 'package:construculator/features/auth/presentation/bloc/auth_bloc/auth_bloc.dart';
 import 'package:construculator/features/auth/presentation/bloc/auth_bloc/auth_state.dart';
 import 'package:construculator/features/estimation/presentation/widgets/cost_estimation_tile.dart';
 import 'package:construculator/features/estimation/presentation/widgets/cost_estimation_empty_page.dart';
 import 'package:construculator/features/estimation/presentation/widgets/project_header_app_bar.dart';
 import 'package:construculator/features/estimation/presentation/widgets/add_estimation_button.dart';
+import 'package:construculator/libraries/mixins/localization_mixin.dart';
+import 'package:construculator/libraries/router/routes/estimation_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -21,21 +24,46 @@ class CostEstimationLandingPage extends StatefulWidget {
       _CostEstimationLandingPageState();
 }
 
-class _CostEstimationLandingPageState extends State<CostEstimationLandingPage> {
+class _CostEstimationLandingPageState extends State<CostEstimationLandingPage>
+    with LocalizationMixin {
   late final AuthBloc _authBloc;
+  late final AddCostEstimationBloc _addCostEstimationBloc;
   String userAvatarUrl = '';
+  String? userId;
 
   @override
   void initState() {
     super.initState();
     _authBloc = Modular.get<AuthBloc>();
+    _addCostEstimationBloc = Modular.get<AddCostEstimationBloc>();
     _authBloc.initialize();
   }
 
   @override
   void dispose() {
     _authBloc.close();
+    _addCostEstimationBloc.close();
     super.dispose();
+  }
+
+  void _createEstimation() {
+    final currentUserId = userId;
+    if (currentUserId == null) {
+      CoreToast.showError(
+        context,
+        l10n?.userIdNotAvailable ?? 'User ID is not available',
+        l10n?.closeLabel ?? 'Close',
+      );
+      return;
+    }
+
+    _addCostEstimationBloc.add(
+      AddCostEstimationSubmitted(
+        estimationName: l10n?.untitledEstimation ?? 'Untitled Estimation',
+        projectId: widget.projectId,
+        creatorUserId: currentUserId,
+      ),
+    );
   }
 
   @override
@@ -46,10 +74,15 @@ class _CostEstimationLandingPageState extends State<CostEstimationLandingPage> {
         if (state is AuthLoadSuccess) {
           setState(() {
             userAvatarUrl = state.avatarUrl ?? '';
+            userId = state.user?.id;
           });
         } else if (state is AuthLoadFailure) {
           if (mounted) {
-            CoreToast.showError(context, state.message, 'Close');
+            CoreToast.showError(
+              context,
+              state.message,
+              l10n?.closeLabel ?? 'Close',
+            );
           }
         }
       },
@@ -81,22 +114,54 @@ class _CostEstimationLandingPageState extends State<CostEstimationLandingPage> {
   }
 
   Widget _buildBody() {
-    return BlocConsumer<CostEstimationListBloc, CostEstimationListState>(
-      listener: (context, state) {
-        if (state is CostEstimationListError) {
-          CoreToast.showError(context, state.message, 'Close');
-        }
-      },
-      builder: (context, state) {
-        return RefreshIndicator(
-          onRefresh: () async {
-            BlocProvider.of<CostEstimationListBloc>(
-              context,
-            ).add(CostEstimationListRefreshEvent(projectId: widget.projectId));
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<CostEstimationListBloc, CostEstimationListState>(
+          listener: (context, state) {
+            if (state is CostEstimationListError) {
+              CoreToast.showError(
+                context,
+                state.message,
+                l10n?.closeLabel ?? 'Close',
+              );
+            }
           },
-          child: _buildContent(state),
-        );
-      },
+        ),
+        BlocListener<AddCostEstimationBloc, AddCostEstimationState>(
+          bloc: _addCostEstimationBloc,
+          listener: (context, state) {
+            if (state is AddCostEstimationSuccess) {
+              Modular.to.pushNamed(
+                '$fullEstimationDetailsRoute/${state.costEstimation.id}',
+              );
+
+              BlocProvider.of<CostEstimationListBloc>(context).add(
+                CostEstimationListRefreshEvent(projectId: widget.projectId),
+              );
+            } else if (state is AddCostEstimationFailure) {
+              if (mounted) {
+                CoreToast.showError(
+                  context,
+                  state.message,
+                  l10n?.closeLabel ?? 'Close',
+                );
+              }
+            }
+          },
+        ),
+      ],
+      child: BlocBuilder<CostEstimationListBloc, CostEstimationListState>(
+        builder: (context, state) {
+          return RefreshIndicator(
+            onRefresh: () async {
+              BlocProvider.of<CostEstimationListBloc>(context).add(
+                CostEstimationListRefreshEvent(projectId: widget.projectId),
+              );
+            },
+            child: _buildContent(state),
+          );
+        },
+      ),
     );
   }
 
@@ -120,15 +185,12 @@ class _CostEstimationLandingPageState extends State<CostEstimationLandingPage> {
   Widget _buildEmptyState() {
     return Stack(
       children: [
-        const CostEstimationEmptyPage(
+        CostEstimationEmptyPage(
           message:
+              l10n?.noEstimationAddedMessage ??
               'No estimation added. To add an estimation please click on add button',
         ),
-        Positioned(
-          bottom: MediaQuery.of(context).size.height * 0.135,
-          right: MediaQuery.of(context).size.width * 0.05,
-          child: AddEstimationButton(onPressed: () {}),
-        ),
+        _buildAddEstimationButton(),
       ],
     );
   }
@@ -137,7 +199,10 @@ class _CostEstimationLandingPageState extends State<CostEstimationLandingPage> {
     return Stack(
       children: [
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: CoreSpacing.space4, vertical: CoreSpacing.space4),
+          padding: const EdgeInsets.symmetric(
+            horizontal: CoreSpacing.space4,
+            vertical: CoreSpacing.space4,
+          ),
           child: ListView.builder(
             itemCount: estimations.length,
             itemBuilder: (context, index) {
@@ -149,12 +214,19 @@ class _CostEstimationLandingPageState extends State<CostEstimationLandingPage> {
             },
           ),
         ),
-        Positioned(
-          bottom: MediaQuery.of(context).size.height * 0.135,
-          right: MediaQuery.of(context).size.width * 0.05,
-          child: AddEstimationButton(onPressed: () {}),
-        ),
+        _buildAddEstimationButton(),
       ],
+    );
+  }
+
+  Widget _buildAddEstimationButton() {
+    return Positioned(
+      bottom: MediaQuery.of(context).size.height * 0.135,
+      right: MediaQuery.of(context).size.width * 0.05,
+      child: Container(
+        color: CoreBackgroundColors.pageBackground,
+        child: AddEstimationButton(onPressed: () => _createEstimation()),
+      ),
     );
   }
 }
