e695e1adb69c93d3c12f74caebad041869a0593b
Simon Dereje <simondereje1>
2026-01-16 15:21:46 +0300

    refactor auth related tests
    CA-461/refactor/refactor-test-directories-for-estimation
    
    CA-461 /infra/change-scripts-in-backaward-comaptible-way

---
+++ test/features/auth/screenshots/auth_provider_buttons_screenshot_test.dart
@@ -0,0 +1,67 @@
+import 'package:construculator/features/auth/presentation/widgets/auth_provider_buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:construculator/l10n/generated/app_localizations.dart';
+
+import '../../../screenshots/font_loader.dart';
+
+void main() {
+  final size = const Size(390, 300);
+  final ratio = 1.0;
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() async {
+    await loadAppFonts();
+  });
+
+  group('AuthProviderButtons Screenshot Tests', () {
+    Future<void> pumpAuthProviderButtons({
+      required WidgetTester tester,
+      required bool isEmailAuth,
+    }) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: createTestTheme(),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: Scaffold(
+            body: Container(
+              margin: const EdgeInsets.only(top: 16),
+              child: AuthProviderButtons(
+                onPressed: (_) {},
+                isEmailAuth: isEmailAuth,
+              ),
+            ),
+          ),
+        ),
+      );
+      await tester.pumpAndSettle();
+    }
+
+    testWidgets('renders email auth mode correctly', (tester) async {
+      tester.view.physicalSize = size;
+      tester.view.devicePixelRatio = ratio;
+      await pumpAuthProviderButtons(tester: tester, isEmailAuth: true);
+
+      await expectLater(
+        find.byType(AuthProviderButtons),
+        matchesGoldenFile(
+          'goldens/auth_provider_buttons/${size.width}x${size.height}/auth_provider_buttons_email_auth.png',
+        ),
+      );
+    });
+
+    testWidgets('renders phone auth mode correctly', (tester) async {
+      tester.view.physicalSize = size;
+      tester.view.devicePixelRatio = ratio;
+      await pumpAuthProviderButtons(tester: tester, isEmailAuth: false);
+
+      await expectLater(
+        find.byType(AuthProviderButtons),
+        matchesGoldenFile(
+          'goldens/auth_provider_buttons/${size.width}x${size.height}/auth_provider_buttons_phone_auth.png',
+        ),
+      );
+    });
+  });
+}
