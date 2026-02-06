129289d219227edf9eb09f84ebcb66ec6ad08b7c
Simon Dereje <134320950+SimonDerejeW@users.noreply.github.com>
2026-02-06 17:22:17 +0300

    Refactor/return void for soft deletes to work (#176)
    * CA-471: refactor: Update deleteEstimation methods to return void instead of CostEstimateDto
    
    * refactor: Remove AuthBloc, events, states, and related tests (#177)
    
    refactor: Update Authbloc dependencies in estimation pages and tets
    
    Co-authored-by: Simon Dereje <simondereje1>
    
    ---------
    
    Co-authored-by: Simon Dereje <simondereje1>

--- test/features/estimations/units/blocs/cost_estimation_list_bloc_test.dart
+++ test/features/estimations/units/blocs/cost_estimation_list_bloc_test.dart
@@ -17,7 +17,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bloc_test/bloc_test.dart';
 
-import '../helpers/estimation_test_data_map_factory.dart';
+import '../../helpers/estimation_test_data_map_factory.dart';
 
 void main() {
   group('CostEstimationListBloc', () {
