90f9f5e433f4968d465b91420b5f79c148d7e01d
bazabizi <bazabizi3@gmail.com>
2026-02-20 15:28:30 +0300

    fix: remove whitespace

--- test/features/auth/widgets/pages/set_new_password_page_test.dart
+++ test/features/auth/widgets/pages/set_new_password_page_test.dart
@@ -19,7 +19,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ripplearc_coreui/ripplearc_coreui.dart';
-
 import '../../../../utils/screenshot/font_loader.dart';
 
 class _SetNewPasswordPageTestModule extends Module {
