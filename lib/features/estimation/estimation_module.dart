301270141078232d4c494f594946dc006e81d7b7
Simon Dereje <simondereje1>
2026-02-25 19:59:04 +0300

    CA-124 feat: Implement cost estimation log repository and data source with pagination.

--- lib/features/estimation/estimation_module.dart
+++ lib/features/estimation/estimation_module.dart
@@ -1,8 +1,12 @@
 import 'package:construculator/app/app_bootstrap.dart';
 import 'package:construculator/features/auth/auth_module.dart';
 import 'package:construculator/features/estimation/data/data_source/interfaces/cost_estimation_data_source.dart';
+import 'package:construculator/features/estimation/data/data_source/interfaces/cost_estimation_log_data_source.dart';
 import 'package:construculator/features/estimation/data/data_source/remote_cost_estimation_data_source.dart';
+import 'package:construculator/features/estimation/data/data_source/remote_cost_estimation_log_data_source.dart';
+import 'package:construculator/features/estimation/data/repositories/cost_estimation_log_repository_impl.dart';
 import 'package:construculator/features/estimation/data/repositories/cost_estimation_repository_impl.dart';
+import 'package:construculator/features/estimation/domain/repositories/cost_estimation_log_repository.dart';
 import 'package:construculator/features/estimation/domain/repositories/cost_estimation_repository.dart';
 import 'package:construculator/features/estimation/domain/usecases/add_cost_estimation_usecase.dart';
 import 'package:construculator/features/estimation/presentation/bloc/cost_estimation_list_bloc/cost_estimation_list_bloc.dart';
@@ -88,8 +92,19 @@ class EstimationModule extends Module {
       ),
     );
 
+    i.addLazySingleton<CostEstimationLogDataSource>(
+      () => RemoteCostEstimationLogDataSource(
+        supabaseWrapper: appBootstrap.supabaseWrapper,
+      ),
+    );
+
     i.addLazySingleton<CostEstimationRepository>(
       () => CostEstimationRepositoryImpl(dataSource: i.get()),
+      config: BindConfig(onDispose: (repository) => repository.dispose()),
+    );
+
+    i.addLazySingleton<CostEstimationLogRepository>(
+      () => CostEstimationLogRepositoryImpl(dataSource: i.get()),
       config: BindConfig(onDispose: (repository) => repository.dispose()),
     );
 
