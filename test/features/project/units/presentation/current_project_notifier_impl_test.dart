c919c6e0dcd4f8ea133149ac9dace94da4cb04ac
Ayana Samuel <ayanasamuel8@gmail.com>
2026-02-26 15:15:01 +0300

    test: add serialization test for ProfessionalRole model

--- test/features/project/units/presentation/current_project_notifier_impl_test.dart
+++ test/features/project/units/presentation/current_project_notifier_impl_test.dart
@@ -1,5 +1,6 @@
 import 'package:construculator/libraries/project/data/current_project_notifier_impl.dart';
 import 'package:construculator/libraries/project/interfaces/current_project_notifier.dart';
+import 'package:construculator/libraries/auth/data/models/professional_role.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -186,6 +187,13 @@ void main() {
 
         await subscription.cancel();
         expect(errorOccurred, isFalse);
+      });
+
+      test('serializes and deserializes generated model code', () {
+        final role = ProfessionalRole(id: 'role-1', name: 'Engineer');
+        final decodedRole = ProfessionalRole.fromJson(role.toJson());
+
+        expect(decodedRole, role);
       });
     });
   });
