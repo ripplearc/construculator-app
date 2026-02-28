df9cc17ea1a3f82d777f6a152f1ff831762e80d3
Ayana Samuel <ayanasamuel8@gmail.com>
2026-02-28 13:02:52 +0300

    refactor: update tests to use FakeTabModuleLoader and improve setup for AppBootstrap

--- test/app/shell/app_shell_page_test.dart
+++ test/app/shell/app_shell_page_test.dart
@@ -1,12 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:construculator/app/app_bootstrap.dart';
 import 'package:construculator/app/shell/app_shell_page.dart';
 import 'package:construculator/app/shell/tab_module_loader.dart';
+import 'package:construculator/libraries/config/testing/fake_app_config.dart';
+import 'package:construculator/libraries/config/testing/fake_env_loader.dart';
+import 'package:construculator/libraries/time/testing/fake_clock_impl.dart';
+import 'package:construculator/libraries/supabase/testing/fake_supabase_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class MockTabModuleLoader extends TabModuleLoader {
-  final List<ShellTab> loadedTabs;
-  MockTabModuleLoader() : loadedTabs = [], super(MockAppBootstrap());
+class FakeTabModuleLoader extends TabModuleLoader {
+  final List<ShellTab> loadedTabs = [];
+  FakeTabModuleLoader(super.bootstrap);
   @override
   Future<void> ensureTabModuleLoaded(ShellTab tab) async {
     loadedTabs.add(tab);
@@ -16,27 +21,38 @@ class MockTabModuleLoader extends TabModuleLoader {
   bool isLoaded(ShellTab tab) => loadedTabs.contains(tab);
 }
 
-class MockAppBootstrap extends AppBootstrap {}
+class _TestModule extends Module {}
 
 void main() {
+  late AppBootstrap appBootstrap;
+  late FakeTabModuleLoader loader;
+
+  setUp(() {
+    appBootstrap = AppBootstrap(
+      config: FakeAppConfig(),
+      envLoader: FakeEnvLoader(),
+      supabaseWrapper: FakeSupabaseWrapper(clock: FakeClockImpl()),
+    );
+    loader = FakeTabModuleLoader(appBootstrap);
+    Modular.init(_TestModule());
+    Modular.bindModule(_TestModule());
+    Modular.replaceInstance<TabModuleLoader>(loader);
+  });
+
+  Widget makeApp() {
+    return MaterialApp(home: AppShellPage());
+  }
+
   testWidgets('AppShellPage loads first tab module in initState', (
     tester,
   ) async {
-    Modular.init(MockModule());
-    Modular.bindModule(MockModule());
-    Modular.bindSingleton<TabModuleLoader>(MockTabModuleLoader());
-    await tester.pumpWidget(const MaterialApp(home: AppShellPage()));
-    // Should show loader for first tab
+    await tester.pumpWidget(makeApp());
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    expect(loader.loadedTabs.contains(ShellTab.home), isTrue);
   });
 
   testWidgets('AppShellPage loads tab module on tab tap', (tester) async {
-    Modular.init(MockModule());
-    Modular.bindModule(MockModule());
-    final loader = MockTabModuleLoader();
-    Modular.bindSingleton<TabModuleLoader>(loader);
-    await tester.pumpWidget(const MaterialApp(home: AppShellPage()));
-    // Simulate tab tap
+    await tester.pumpWidget(makeApp());
     await tester.tap(find.text('Calculations'));
     await tester.pump();
     expect(loader.loadedTabs.contains(ShellTab.calculations), isTrue);
