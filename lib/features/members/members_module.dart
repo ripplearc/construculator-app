ec932a3adda23899d714065fdca596c7e32c20fb
Ayana Samuel <ayanasamuel8@gmail.com>
2026-03-02 11:14:32 +0100

    refactor: remove AppBootstrap dependency from MembersModule

--- lib/features/members/members_module.dart
+++ lib/features/members/members_module.dart
@@ -1,11 +1,8 @@
-import 'package:construculator/app/app_bootstrap.dart';
 import 'package:construculator/features/members/presentation/pages/members_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class MembersModule extends Module {
-  final AppBootstrap appBootstrap;
-
-  MembersModule(this.appBootstrap);
+  MembersModule();
 
   @override
   void routes(RouteManager r) {
