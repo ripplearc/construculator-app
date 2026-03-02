bfc77a1f0db97899b591dfcde7f343cfb9f2ccef
Ayana Samuel <ayanasamuel8@gmail.com>
2026-03-02 10:55:47 +0100

    refactor: simplify CalculationsModule by removing AppBootstrap dependency

--- lib/features/calculations/calculations_module.dart
+++ lib/features/calculations/calculations_module.dart
@@ -1,11 +1,8 @@
-import 'package:construculator/app/app_bootstrap.dart';
 import 'package:construculator/features/calculations/presentation/pages/calculations_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class CalculationsModule extends Module {
-  final AppBootstrap appBootstrap;
-
-  CalculationsModule(this.appBootstrap);
+  CalculationsModule();
 
   @override
   void routes(RouteManager r) {
