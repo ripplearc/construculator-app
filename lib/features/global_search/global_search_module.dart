b3c8194531f6df6c4e75c56cad38a460bbc038dc
bazabizi <bazabizi3@gmail.com>
2026-03-26 11:09:47 +0300

    chore(modules): register global search data source

---
+++ lib/features/global_search/global_search_module.dart
@@ -0,0 +1,26 @@
+import 'package:construculator/app/app_bootstrap.dart';
+import 'package:construculator/features/global_search/data/data_source/interfaces/global_search_data_source.dart';
+import 'package:construculator/features/global_search/data/data_source/remote_global_search_data_source.dart';
+import 'package:construculator/libraries/supabase/supabase_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+/// Module for the global search feature.
+///
+/// Provides [GlobalSearchDataSource] binding for dependency injection.
+class GlobalSearchModule extends Module {
+  final AppBootstrap appBootstrap;
+
+  GlobalSearchModule(this.appBootstrap);
+
+  @override
+  List<Module> get imports => [SupabaseModule(appBootstrap)];
+
+  @override
+  void binds(Injector i) {
+    i.addLazySingleton<GlobalSearchDataSource>(
+      () => RemoteGlobalSearchDataSource(
+        supabaseWrapper: appBootstrap.supabaseWrapper,
+      ),
+    );
+  }
+}
