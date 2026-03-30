8746ff0322eecfdc2c4060df0a19d79b8773b724
eyoaab <eyobtariku48@gmail.com>
2026-03-31 00:26:36 +0300

    R4:simplify splash screen implementation and remove unused assets

--- android/app/src/main/kotlin/com/cms/cm_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/cms/cm_sample/MainActivity.kt
@@ -1,38 +1,11 @@
 package com.cms.cm_sample
 
-import android.os.Bundle
-import android.graphics.drawable.AnimationDrawable
-import android.widget.ImageView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashScreen = installSplashScreen()
-        
-        // Start the animation when splash screen is shown
-        splashScreen.setOnExitAnimationListener { splashScreenView ->
-            // Get the animated drawable and start it
-            val splashIconView = splashScreenView.iconView as? ImageView
-            val drawable = splashIconView?.drawable
-            
-            if (drawable is AnimationDrawable) {
-                drawable.start()
-                
-                // Calculate total animation duration
-                val totalDuration = (0 until drawable.numberOfFrames)
-                    .sumOf { drawable.getDuration(it).toLong() }
-                
-                // Remove splash screen after animation completes
-                splashIconView.postDelayed({
-                    splashScreenView.remove()
-                }, totalDuration)
-            } else {
-                // Fallback: remove immediately if not an animation
-                splashScreenView.remove()
-            }
-        }
-        
+        installSplashScreen()
         super.onCreate(savedInstanceState)
     }
 }
