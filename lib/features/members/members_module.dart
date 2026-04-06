a6413ac86d8bdc9874b42d1ab5caa820c0a87e0f
Ayana Samuel <ayanasamuel8@gmail.com>
2026-04-06 23:18:09 +0300

    feat: add members localization and update MembersPage to use localized string

--- lib/features/members/members_module.dart
+++ lib/features/members/members_module.dart
@@ -1,6 +1,7 @@
 import 'package:construculator/features/members/presentation/pages/members_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+/// Flutter Modular module for the Members feature.
 class MembersModule extends Module {
   MembersModule();
 
