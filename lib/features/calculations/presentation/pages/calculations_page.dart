ea013d80ae03010183dfa2d6e803ac87c57ed7cf
Ayana Samuel <ayanasamuel8@gmail.com>
2026-04-06 23:11:14 +0300

    feat: register calculations module and localize calculations page label

--- lib/features/calculations/presentation/pages/calculations_page.dart
+++ lib/features/calculations/presentation/pages/calculations_page.dart
@@ -1,6 +1,9 @@
 import 'package:construculator/libraries/extensions/extensions.dart';
 import 'package:flutter/material.dart';
 
+/// Placeholder page for the Calculations tab.
+///
+/// Displays a centered label until the full Calculations feature is implemented.
 class CalculationsPage extends StatelessWidget {
   const CalculationsPage({super.key});
 
@@ -8,7 +11,7 @@ class CalculationsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        'Calculations',
+        context.l10n.calculations,
         style: context.textTheme.headlineMediumSemiBold,
       ),
     );
