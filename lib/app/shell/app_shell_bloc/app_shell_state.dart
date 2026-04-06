a565844b240eee3eb8c935008e9d2495eecf74bf
Ayana Samuel <ayanasamuel8@gmail.com>
2026-04-06 22:56:35 +0300

    refactor: remove Modular dependency from AppShellBloc and add documentation comments

--- lib/app/shell/app_shell_bloc/app_shell_state.dart
+++ lib/app/shell/app_shell_bloc/app_shell_state.dart
@@ -1,7 +1,12 @@
 part of 'app_shell_bloc.dart';
 
+/// Represents the state of the app shell, including the currently selected tab
+/// and the set of tabs that have been loaded.
 class AppShellState extends Equatable {
+  /// The index of the currently active tab.
   final int selectedTabIndex;
+
+  /// The set of indices of all tabs that have been loaded.
   final Set<int> loadedTabIndexes;
 
   const AppShellState({
@@ -9,6 +14,7 @@ class AppShellState extends Equatable {
     required this.loadedTabIndexes,
   });
 
+  /// Creates a copy of this state, replacing the provided properties with new values.
   AppShellState copyWith({int? selectedTabIndex, Set<int>? loadedTabIndexes}) {
     return AppShellState(
       selectedTabIndex: selectedTabIndex ?? this.selectedTabIndex,
