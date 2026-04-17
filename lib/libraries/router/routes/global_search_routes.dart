5ea5bc2427c1926de5eaada705104e73d372f5ba
Natnael Tafesse <bazabizi3@gmail.com>
2026-04-17 11:28:19 +0300

    R1: fix(global-search): move Modular.get to field init, add doc comments, fix TODO format

--- lib/libraries/router/routes/global_search_routes.dart
+++ lib/libraries/router/routes/global_search_routes.dart
@@ -1,3 +1,8 @@
+/// Base route segment for the global search feature module.
 const String globalSearchBaseRoute = '/search';
+
+/// Route segment for the [GlobalSearchPage] within [GlobalSearchModule].
 const String globalSearchPageRoute = '/';
+
+/// Full routable path to [GlobalSearchPage].
 const String fullGlobalSearchRoute = '$globalSearchBaseRoute$globalSearchPageRoute';
