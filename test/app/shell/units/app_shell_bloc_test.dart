a70410cd4784a63f0f56eea842ea83cd578b6c1f
Ayana Samuel <ayanasamuel8@gmail.com>
2026-04-27 19:05:58 +0300

    test: refactor AppShellBloc tests to use Modular for dependency injection

--- test/app/shell/units/app_shell_bloc_test.dart
+++ test/app/shell/units/app_shell_bloc_test.dart
@@ -1,6 +1,6 @@
-// ignore_for_file: no_direct_instantiation
 import 'package:bloc_test/bloc_test.dart';
 import 'package:construculator/app/shell/app_shell_bloc/app_shell_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -58,7 +58,7 @@ void main() {
 
     blocTest<AppShellBloc, AppShellState>(
       'updates selected tab and tracks lazy-loaded tabs',
-      build: () => AppShellBloc(),
+      build: () => Modular.get<AppShellBloc>(),
       act: (bloc) {
         bloc.add(const AppShellTabSelected(1));
         bloc.add(const AppShellTabSelected(3));
@@ -71,7 +71,7 @@ void main() {
 
     blocTest<AppShellBloc, AppShellState>(
       'does not emit when selecting current tab',
-      build: () => AppShellBloc(),
+      build: () => Modular.get<AppShellBloc>(),
       act: (bloc) => bloc.add(const AppShellTabSelected(0)),
       expect: () => <AppShellState>[],
     );
