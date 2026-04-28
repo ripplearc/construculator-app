c5c34dc5f9f0aceb6d7fe72ba3135e67f88033b1
Simon Dereje <simondereje1>
2026-04-28 18:09:56 +0300

    R1: address critica isssues

--- lib/features/estimation/estimation_module.dart
+++ lib/features/estimation/estimation_module.dart
@@ -1,6 +1,8 @@
 import 'package:construculator/app/app_bootstrap.dart';
 import 'package:construculator/features/estimation/data/data_source/interfaces/cost_estimation_log_data_source.dart';
+import 'package:construculator/features/estimation/data/data_source/interfaces/cost_item_data_source.dart';
 import 'package:construculator/features/estimation/data/data_source/remote_cost_estimation_log_data_source.dart';
+import 'package:construculator/features/estimation/data/data_source/remote_cost_item_data_source.dart';
 import 'package:construculator/features/estimation/data/repositories/cost_estimation_log_repository_impl.dart';
 import 'package:construculator/features/estimation/domain/repositories/cost_estimation_log_repository.dart';
 import 'package:construculator/features/estimation/domain/usecases/add_cost_estimation_usecase.dart';
@@ -72,6 +74,12 @@ class EstimationModule extends Module {
   void binds(Injector i) {
     i.addLazySingleton<CostEstimationLogDataSource>(
       () => RemoteCostEstimationLogDataSource(
+        supabaseWrapper: appBootstrap.supabaseWrapper,
+      ),
+    );
+
+    i.addLazySingleton<CostItemDataSource>(
+      () => RemoteCostItemDataSource(
         supabaseWrapper: appBootstrap.supabaseWrapper,
       ),
     );
