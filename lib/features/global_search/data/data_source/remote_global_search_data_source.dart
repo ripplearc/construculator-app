50fef14c722d530833eab5c781443b4ee0c37aef
Ayana Samuel <ayanasamuel8@gmail.com>
2026-04-30 13:07:39 +0300

    refactor: reorganize estimation module imports and enhance repository implementation

--- lib/features/global_search/data/data_source/remote_global_search_data_source.dart
+++ lib/features/global_search/data/data_source/remote_global_search_data_source.dart
@@ -1,9 +1,9 @@
-import 'package:construculator/libraries/estimation/data/models/cost_estimate_dto.dart';
 import 'package:construculator/features/global_search/data/data_source/interfaces/global_search_data_source.dart';
 import 'package:construculator/features/global_search/data/models/search_params_dto.dart';
 import 'package:construculator/features/global_search/data/models/search_results_dto.dart';
 import 'package:construculator/features/global_search/data/models/search_scope.dart';
 import 'package:construculator/libraries/auth/data/models/user_profile_dto.dart';
+import 'package:construculator/libraries/estimation/data/models/cost_estimate_dto.dart';
 import 'package:construculator/libraries/logging/app_logger.dart';
 import 'package:construculator/libraries/project/data/models/project_dto.dart';
 import 'package:construculator/libraries/supabase/database_constants.dart';
