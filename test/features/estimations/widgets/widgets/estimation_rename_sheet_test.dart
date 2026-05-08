a6a143f7d74efd4724ffba153bdbc5d846bc573a
Ayana Samuel <ayanasamuel8@gmail.com>
2026-05-08 14:02:05 +0300

    refactor: update import statements in estimation tests to maintain consistency

--- test/features/estimations/widgets/widgets/estimation_rename_sheet_test.dart
+++ test/features/estimations/widgets/widgets/estimation_rename_sheet_test.dart
@@ -22,9 +22,9 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ripplearc_coreui/ripplearc_coreui.dart';
 
+import '../../../../libraries/estimation/helpers/estimation_test_data_map_factory.dart';
 import '../../../../utils/fake_app_bootstrap_factory.dart';
 import '../../../../utils/screenshot/font_loader.dart';
-import '../../../../libraries/estimation/helpers/estimation_test_data_map_factory.dart';
 
 class _EstimationRenameSheetTestModule extends Module {
   final AppBootstrap appBootstrap;
