bc058cc6ff228a00f13f6ef561bb4acfd29052d7
Ayana Samuel <ayanasamuel8@gmail.com>
2026-05-10 22:37:40 +0300

    refactor: reorder imports and simplify constructor formatting in WatchRecentEstimationsUseCase

--- lib/features/dashboard/domain/usecases/watch_recent_estimations_usecase.dart
+++ lib/features/dashboard/domain/usecases/watch_recent_estimations_usecase.dart
@@ -1,8 +1,8 @@
 import 'package:construculator/libraries/either/either.dart';
 import 'package:construculator/libraries/errors/failures.dart';
-import 'package:construculator/libraries/estimation/domain/estimation_error_type.dart';
 import 'package:construculator/libraries/estimation/domain/entities/cost_estimate_entity.dart';
 import 'package:construculator/libraries/estimation/domain/enums/estimation_sort_option.dart';
+import 'package:construculator/libraries/estimation/domain/estimation_error_type.dart';
 import 'package:construculator/libraries/estimation/domain/repositories/cost_estimation_repository.dart';
 import 'package:construculator/libraries/project/interfaces/current_project_notifier.dart';
 import 'package:equatable/equatable.dart';
@@ -31,10 +31,7 @@ class WatchRecentEstimationsUseCase {
 
   /// Creates a [WatchRecentEstimationsUseCase] with the given [repository]
   /// and [currentProjectNotifier].
-  WatchRecentEstimationsUseCase(
-    this._repository,
-    this._currentProjectNotifier,
-  );
+  WatchRecentEstimationsUseCase(this._repository, this._currentProjectNotifier);
 
   /// Executes the use case to start streaming recent estimations
   Stream<Either<Failure, List<CostEstimate>>> call(
