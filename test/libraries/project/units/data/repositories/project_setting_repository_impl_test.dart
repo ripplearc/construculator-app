c46debfbef9fc12d24889d417770deb1b283dd1d
Ayana Samuel <ayanasamuel8@gmail.com>
2026-05-30 22:47:21 +0300

    refactor: extract _logFailure helper and fix stream error test assertions

--- test/libraries/project/units/data/repositories/project_setting_repository_impl_test.dart
+++ test/libraries/project/units/data/repositories/project_setting_repository_impl_test.dart
@@ -18,6 +18,7 @@ import 'package:construculator/libraries/supabase/testing/fake_supabase_wrapper.
 import 'package:construculator/libraries/time/testing/fake_clock_impl.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' show AuthException;
 
 import '../../../../../utils/fake_app_bootstrap_factory.dart';
 
