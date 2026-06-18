b4e6a26568c012ea809c74a7f1abe42245751051
Simon Dereje <simondereje1>
2026-06-18 21:11:45 +0300

    CA-648: feat: listen to auth events to cleanly connect powersync

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:construculator/libraries/config/env_constants.dart';
 import 'package:construculator/libraries/config/env_loader_impl.dart';
 import 'package:construculator/libraries/logging/app_logger.dart';
 import 'package:construculator/libraries/sentry/sentry_sdk_impl.dart';
+import 'package:construculator/libraries/powersync/data/open_powersync_database.dart';
 import 'package:construculator/libraries/sentry/sentry_wrapper_impl.dart';
 import 'package:construculator/libraries/supabase/supabase_wrapper_impl.dart';
 import 'package:flutter/material.dart';
@@ -38,16 +39,13 @@ Future<AppBootstrap> _initializeApp() async {
   await config.initialize(env);
   final wrapper = SupabaseWrapperImpl(envLoader: envLoader);
   await wrapper.initialize();
-  final sentryWrapper = SentryWrapperImpl(
-    envLoader: envLoader,
-    config: config,
-    sentrySdk: SentrySdkImpl(),
-  );
+  final sentryWrapper = SentryWrapperImpl(envLoader: envLoader, config: config);
   return AppBootstrap(
     config: config,
     envLoader: envLoader,
     supabaseWrapper: wrapper,
     sentryWrapper: sentryWrapper,
+    powerSyncDatabase: powerSyncDatabase,
   );
 }
 
