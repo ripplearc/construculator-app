c26d6cdc3c244fde69fe390f93b4046d2a80dc44
Ayana Samuel <ayanasamuel8@gmail.com>
2026-09-21 22:47:08 +0300

    Move calculator_route_exclusion_test.dart into test/app/shell/widgets/
    run_check.sh only discovers tests inside units/ or widgets/
    subdirectories; sitting directly under test/app/shell/, this test
    never actually ran through the script's test steps, so
    feature_unavailable_module.dart and feature_unavailable_page.dart
    had no coverage record for the --pre gate to find.

--- test/app/shell/widgets/calculator_route_exclusion_test.dart
+++ test/app/shell/widgets/calculator_route_exclusion_test.dart
@@ -18,8 +18,8 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ripplearc_coreui/ripplearc_coreui.dart';
 
-import '../../utils/fake_app_bootstrap_factory.dart';
-import '../../utils/feature_availability_scope.dart';
+import '../../../utils/fake_app_bootstrap_factory.dart';
+import '../../../utils/feature_availability_scope.dart';
 
 /// Exclusion tests for the calculator route (CA-930).
 ///
